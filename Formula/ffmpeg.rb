e8f2dc70642077cb50c2841b835eef439c94af0b
George Carlson <74246849+georgemcarlson@users.noreply.github.com>
2022-05-19 23:56:07 -0500

    Update ffmpeg.rb

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,7 +1,7 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-snapshot.tar.xz"
+  url "https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2"
   version "5.0.1-with-options" # to distinguish from homebrew-core's ffmpeg
   license "GPL-2.0-or-later"
   revision 1
